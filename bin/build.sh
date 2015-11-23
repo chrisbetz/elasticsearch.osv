46252659c914b18296eb8dfef21b8334c79fccfb
Christian Betz <christian.betz@performance-media.de>
2015-11-23 12:51:21 +0100

    remove local path reference

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
 capstan delete elasticsearch.osv
 capstan build -v -p vbox
-VBoxManage internalcommands sethduuid /Users/cbetz/.capstan/repository/elasticsearch.osv/elasticsearch.osv.vbox 6ab12522-1dfd-412a-82a7-ddb6cbe6f2fd
+VBoxManage internalcommands sethduuid ~/.capstan/repository/elasticsearch.osv/elasticsearch.osv.vbox 6ab12522-1dfd-412a-82a7-ddb6cbe6f2fd
